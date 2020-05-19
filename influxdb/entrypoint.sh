e90ba20cadeec8c3618a9698f163982c7593e135
etom2890 <etom2890@gmail.com>
2020-05-19 18:25:28 -0400

    Signed-off-by: etom2890 <etom2890@gmail.com>

--- influxdb/entrypoint.sh
+++ influxdb/entrypoint.sh
@@ -2,7 +2,7 @@
 
 
 if [ ! -f "/var/lib/influxdb/.init" ]; then
-    exec influxd -config /usr/local/etc/influxdb.conf $@ &
+    exec influxd  $@ &
 	
 
 
@@ -27,5 +27,4 @@ if [ ! -f "/var/lib/influxdb/.init" ]; then
 fi
 
 exec influxd -config /usr/local/etc/influxdb.conf $@
-	influx -host 'localhost' -port '8086' -execute 'CREATE USER ${INFLUX_USER} WITH PASSWORD '${INFLUX_PASSWORD}' WITH ALL PRIVILEGES'
-	influx -host 'localhost' -port '8086' -execute 'CREATE DATABASE ${INFLUX_DB}' 
+	
