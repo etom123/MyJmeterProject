a99df36cdfe6c623342392a1d0fe712e94f8b11c
etom2890 <etom2890@gmail.com>
2020-05-19 17:03:14 -0400

    Signed-off-by: etom2890 <etom2890@gmail.com>

--- influxdb/entrypoint.sh
+++ influxdb/entrypoint.sh
@@ -3,6 +3,9 @@
 
 if [ ! -f "/var/lib/influxdb/.init" ]; then
     exec influxd -config /usr/local/etc/influxdb.conf $@ &
+	
+	influx -host=localhost -port=8086 -execute="CREATE USER ${INFLUX_USER} WITH PASSWORD '${INFLUX_PASSWORD}' WITH ALL PRIVILEGES"
+    influx -host=localhost -port=8086 -execute="CREATE DATABASE ${INFLUX_DB}" 
 
     until wget -q "http://localhost:8086/ping" 2> /dev/null; do
         sleep 1
@@ -14,8 +17,7 @@ if [ ! -f "/var/lib/influxdb/.init" ]; then
 	
 	
 	exec influxd -config /usr/local/etc/influxdb.conf $@ 
-	influx -host=localhost -port=8086 -execute="CREATE USER ${INFLUX_USER} WITH PASSWORD '${INFLUX_PASSWORD}' WITH ALL PRIVILEGES"
-    influx -host=localhost -port=8086 -execute="CREATE DATABASE ${INFLUX_DB}" 
+	
     
 	touch "/var/lib/influxdb/.init"
 
